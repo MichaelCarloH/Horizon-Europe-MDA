36d9894aebac4aec18f04cdd5f36b7ff5d926cc7
michaelcarloH <michael.carlo@outlook.it>
2025-05-08 13:52:07 +0200

    Fix deployment by creating venv in Azure

--- backend/startup.sh
+++ backend/startup.sh
@@ -3,8 +3,17 @@ set -e
 
 cd /home/site/wwwroot
 
+echo "Creating virtual environment..."
+python -m venv venv
+
 echo "Activating virtual environment..."
 source venv/bin/activate
+
+echo "Upgrading pip..."
+python -m pip install --upgrade pip
+
+echo "Installing dependencies..."
+pip install --no-cache-dir -r requirements.txt
 
 echo "Starting application..."
 exec python -m gunicorn --config gunicorn.conf.py api:app 
