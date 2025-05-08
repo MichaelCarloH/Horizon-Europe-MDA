932ec8e5dba578c9037f4e9794dec30073dca735
michaelcarloH <michael.carlo@outlook.it>
2025-05-08 13:47:33 +0200

    langchain added

--- backend/startup.sh
+++ backend/startup.sh
@@ -3,16 +3,8 @@ set -e
 
 cd /home/site/wwwroot
 
-echo "Creating virtual environment..."
-python -m venv venv
+echo "Activating virtual environment..."
 source venv/bin/activate
-
-echo "Upgrading pip..."
-python -m pip install --upgrade pip
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-pip install gunicorn
 
 echo "Starting application..."
 exec python -m gunicorn --config gunicorn.conf.py api:app 
