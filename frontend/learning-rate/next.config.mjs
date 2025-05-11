eabd714eebd19543b8eac2332c41e2e4a86395c3
Keith Atienza <keithatienza@Keiths-Laptop.local>
2025-05-12 00:18:51 +0200

    updated backend script with self querying retrieval

--- frontend/learning-rate/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ['smallneuron'],
-
-};
-
-export default nextConfig;
--- frontend/learning-rate/src/middleware/csrf.ts
+++
@@ -1,8 +0,0 @@
-import { createCsrfMiddleware, createCsrfProtect } from '@edge-csrf/nextjs';
-
-// initalize csrf protection middleware
-export default createCsrfProtect({
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-  },
-});
